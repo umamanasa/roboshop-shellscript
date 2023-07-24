89910c22b7f2d4db335b8c811f69f83259f868e4
umamanasa <umamanasa49@gmail.com>
2023-07-24 23:06:17 +0530

    functions in shipping component

--- common.sh
+++ common.sh
@@ -1,4 +1,8 @@
+log=/tmp/roboshop.log
 func_apppreq(){
+    echo -e "\e[36m>>>>>> Create ${component} service <<<<<\e[0m"
+      cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+
   echo -e "\e[36m>>>>>> Create Application User <<<<<\e[0m"
     useradd roboshop &>>${log}
 
@@ -26,8 +30,6 @@ func_systemd(){
 }
 func_nodejs(){
   log=/tmp/roboshop.log
-  echo -e "\e[36m>>>>>> Create ${component} service <<<<<\e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
 
   echo -e "\e[36m>>>>>> Create MongoDB Repo <<<<<\e[0m"
 
@@ -56,24 +58,36 @@ func_nodejs(){
 
 func_java(){
   echo -e "\e[36m>>>>>> Create ${component} service <<<<<\e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
 
   echo -e "\e[36m>>>>>> install maven <<<<<\e[0m"
-  yum install maven -y
+  yum install maven -y &>>${log}
 
   func_apppreq
 
   echo -e "\e[36m>>>>>> Build ${component} service <<<<<\e[0m"
-  mvn clean package
-  mv target/${component}-1.0.jar ${component}.jar
+  mvn clean package &>>${log}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log}
 
 
   echo -e "\e[36m>>>>>> Install MYSQL Client <<<<<\e[0m"
-  yum install mysql -y
+  yum install mysql -y &>>${log}
 
   echo -e "\e[36m>>>>>> Load Schema <<<<<\e[0m"
-  mysql -h mysql.manasareddy.online -uroot -pRoboShop@1 < /app/schema/${component}.sql
+  mysql -h mysql.manasareddy.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
 
   func_systemd
+}
 
+func_python(){
+
+  echo -e "\e[36m>>>>>> Build ${component} Service <<<<<\e[0m"
+  yum install python36 gcc python3-devel -y &>>${log}
+
+  func_apppreq
+
+  echo -e "\e[36m>>>>>> Build ${component} Service <<<<<\e[0m"
+  pip3.6 install -r requirements.txt &>>${log}
+
+  func_systemd
 }
