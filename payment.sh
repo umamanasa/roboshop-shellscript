eba0b52311345ad8de8379fa4bbd606b97f950e7
umamanasa <umamanasa49@gmail.com>
2023-07-25 13:55:12 +0530

    root password in payment

--- payment.sh
+++ payment.sh
@@ -1,6 +1,10 @@
 component=payment
 source common.sh
-
+rabbitmq_app_password=$1
+if [ -z "${rabbitmq_app_password}" ]; then
+  echo Input RabbitMQ Appuser password Missing
+  exit 1
+  fi
 func_python
 
 
