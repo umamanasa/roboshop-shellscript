cb9628008a1a4b7e8eaa8ff91ddecaa5717e8529
umamanasa <umamanasa49@gmail.com>
2023-07-25 12:26:07 +0530

    conditions in cart service

--- common.sh
+++ common.sh
@@ -2,23 +2,28 @@ log=/tmp/roboshop.log
 func_apppreq(){
     echo -e "\e[36m>>>>>> Create ${component} service <<<<<\e[0m"
       cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+      echo $?
 
   echo -e "\e[36m>>>>>> Create Application User <<<<<\e[0m"
     useradd roboshop &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>> Removing Previous Content <<<<<\e[0m"
     rm -rf /app &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>> Create Application Directory <<<<<\e[0m"
     mkdir /app &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>> Download Application Content <<<<<\e[0m"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>> Extract Application Content <<<<<\e[0m"
     cd /app
     unzip /tmp/${component}.zip &>>${log}
-    cd /app
+    echo $?
 
 }
 
@@ -27,22 +32,27 @@ func_systemd(){
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
+    echo $?
 }
 
 func_schema_setup(){
   if ["${schema_type}" == "mongodb"]; then
   echo -e "\e[36m>>>>>> Install Mongo Client <<<<<\e[0m"
     yum install mongodb-org-shell -y &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>> Load ${component} Schema <<<<<\e[0m"
     mongo --host mongodb.manasareddy.online </app/schema/${component}.js &>>${log}
+    echo $?
     fi
     if ["${schema_type}" == "mysql"]; then
     echo -e "\e[36m>>>>>> Install MYSQL Client <<<<<\e[0m"
       yum install mysql -y &>>${log}
+      echo $?
 
       echo -e "\e[36m>>>>>> Load Schema <<<<<\e[0m"
       mysql -h mysql.manasareddy.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+      echo $?
       fi
 }
 
@@ -50,19 +60,22 @@ func_nodejs(){
   log=/tmp/roboshop.log
 
   echo -e "\e[36m>>>>>> Create MongoDB Repo <<<<<\e[0m"
-
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  echo $?
 
   echo -e "\e[36m>>>>>> Install NodeJS Repos <<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+  echo $?
 
   echo -e "\e[36m>>>>>> Install NodeJS <<<<<\e[0m"
   yum install nodejs -y &>>${log}
+  echo $?
 
   func_apppreq
 
   echo -e "\e[36m>>>>>> Downlaod NodeJS Dependencies<<<<<<\e[0m"
   npm install &>>${log}
+  echo $?
 
   func_schema_setup
 
@@ -73,15 +86,18 @@ func_nodejs(){
 func_java(){
   echo -e "\e[36m>>>>>> Create ${component} service <<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  echo $?
 
   echo -e "\e[36m>>>>>> install maven <<<<<\e[0m"
   yum install maven -y &>>${log}
+  echo $?
 
   func_apppreq
 
   echo -e "\e[36m>>>>>> Build ${component} service <<<<<\e[0m"
   mvn clean package &>>${log}
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  echo $?
 
   func_schema_setup
 
@@ -92,11 +108,13 @@ func_python(){
 
   echo -e "\e[36m>>>>>> Build ${component} Service <<<<<\e[0m"
   yum install python36 gcc python3-devel -y &>>${log}
+  echo $?
 
   func_apppreq
 
   echo -e "\e[36m>>>>>> Build ${component} Service <<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
+  echo $?
 
   func_systemd
 }
